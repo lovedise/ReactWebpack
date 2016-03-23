f851265d05652fa9653d5768f7cd20f6b822d1b9
lovedise <lovedise@gmail.com>
2016-03-23 19:23:57 +0900

    init

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+/**
+ * Created by namee on 2016. 3. 23..
+ */
+import React from 'react';
+
+export default class App extends React.Component{
+  render() {
+    return (
+      <div>hello react</div>
+    )
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by namee on 2016. 3. 23..
+ */
+import React from 'react';
+import {render} from 'react-dom';
+import App from 'components/app';
+
+render(<App />, document.getElementById('app'));
